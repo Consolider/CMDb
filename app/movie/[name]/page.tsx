7daee7c006de5aa42960c8b6cc1eaaf55353e5ca
Consolider <christian.spector@protonmail.com>
2025-10-14 21:11:31 +0200

    refactor code

--- app/movie/[name]/page.tsx
+++ app/movie/[name]/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
 import NavBar from "@/components/navbar";
-import style from "./page.module.css"
+import styles from "@/app/movie/[name]/page.module.css"
 import { roundToNearest, runtimeConv, toURL } from "@/lib/utils";
-import { fetchMovieDetails } from "@/lib/data/api-data";
-import { MovieDetails } from "@/lib/interfaces";
+import { fetchMovieCredits, fetchMovieDetails } from "@/lib/data/api-data";
+import { MovieCredits, MovieDetails } from "@/lib/interfaces";
+import Scroller from "@/components/scroller";
 import CardActor from "@/components/card-actor";
 
 export default async function Movie({
@@ -15,6 +16,7 @@ export default async function Movie({
   const [movie_id] = awaitedParams.name.split('-'); // 'id-title' => ['id', 'title']
   // const [movie_id] = params.name.split('-'); // 'id-title' => ['id', 'title']
   const movieDetails: MovieDetails | null = await fetchMovieDetails(Number(movie_id));  
+  const MovieCredits: MovieCredits | null = await fetchMovieCredits(Number(movie_id));  
   
   // "Needed" for "possibly null" prevention
   if (!movieDetails) {
@@ -27,23 +29,20 @@ export default async function Movie({
       </section>
     )
   }
+  if (!MovieCredits) {
+    return (
+      <section className="absolute text-center">
+        <h4>Page - Movie</h4>
+        <div className="relative">
+          <p>No actor data available.</p>
+        </div>
+      </section>
+    )
+  }
 
   return (
     <header
-      className="relative w-[90%] h-[90%] rounded-[20px] overflow-hidden shadow-[0_10px_25px_8px_rgba(0,0,0,0.25)]
-      before:content-[''] before:absolute before:w-[100%] before:h-[100%] before:bg-black
-      before:bg-center
-      before:bg-no-repeat
-      before:bg-cover
-      before:opacity-[.6]
-      before:z-[-1]
-      after:content-['']
-      after:absolute
-      after:w-[100%]
-      after:h-[100%]
-      after:bg-linear-[180deg,transparent,black]
-      after:z-[-1]
-      ">
+      className="relative w-[90%] h-[90%] rounded-[20px] overflow-hidden shadow-[0_10px_25px_8px_rgba(0,0,0,0.25)] before:content-[''] before:absolute before:w-[100%] before:h-[100%] before:bg-black before:bg-center before:bg-no-repeat before:bg-cover before:opacity-[.6] before:z-[-1] after:content-[''] after:absolute after:w-[100%] after:h-[100%] after:bg-linear-[180deg,transparent,black] after:z-[-1]">
       <Image
         loading="eager"
         className="absolute bg-center bg-no-repeat bg-cover opacity-[.6] z-[-1] bg-linear-[180deg,transparent,black]"
@@ -54,10 +53,10 @@ export default async function Movie({
       />
       <NavBar />
 
-      <div className={style.content}>
+      <div className={styles.content}>
         <h1>{movieDetails.title}</h1>
         <p>{movieDetails.overview}</p>
-        <div className={style.details}>
+        <div className={styles.details}>
           <h2>Movie</h2>
           <h3>{movieDetails.release_date}</h3>
           {movieDetails.genres.map((genre, index) => (
@@ -67,7 +66,18 @@ export default async function Movie({
           <h5><span>IMDb</span> {roundToNearest(movieDetails.vote_average, 1)}</h5>
         </div>
       </div>
-      <CardActor movie_id={Number(movie_id)}/>
+
+      <h2 className={styles.container_heading}>Actors</h2>
+        <section className={styles.container}>
+        {/* <div className={styles.cards}> */}
+          <Scroller>
+          {MovieCredits.cast.map((data: any, index: number) => (
+            <CardActor key={index} data={data} />
+          ))}
+          </Scroller>
+        {/* </div> */}
+        </section>
+      {/* <CardActor movie_id={Number(movie_id)}/> */}
     </header>
   )
 }
--- components/card-actor.tsx
+++ components/card-actor.tsx
@@ -1,67 +1,38 @@
 import Link from "next/link"
 import Image from "next/image"
-import styles from "./card-actor.module.css"
+import styles from "@/components/card-actor.module.css"
 import { toURL } from "@/lib/utils";
 import { ChevronLeftCircle, ChevronRightCircle } from "lucide-react";
 import { fetchMovieCredits } from "@/lib/data/api-data";
 import { MovieCredits } from "@/lib/interfaces";
 
-interface CardActorProps {
-    movie_id: number;
-}
-
-export default async function CardActor({ movie_id }: CardActorProps) {
-  const credits: MovieCredits | null = await fetchMovieCredits(movie_id);
-
-  // "Needed" for "possibly null" prevention
-  if (!credits) {
-    return (
-      // <section className="absolute text-center">
-      <section className={styles.section}>
-        <h4>Card-Actor</h4>
-        <div className="relative">
-          <p>No actor data available.</p>
-        </div>
-      </section>
-    )
-  }
-
+export default function CardActor({ data }: { data: any }) {
   return (
-    <section className={styles.section}>
-      <h4>Actors</h4>
-      <ChevronLeftCircle size={20} className={styles.bi_chevron_left} />
-      <ChevronRightCircle size={20} className={styles.bi_chevron_right} />
-      <div className={styles.cards}>
-        {credits.cast.map((data, index) => (
-            <Link
-              key={index}
-              className={styles.card}
-              href={`/actor/${data.id}-${toURL(data.name)}`}
-            > 
-              <Image
-              className={styles.poster}
-              src={`https://image.tmdb.org/t/p/original/${data.profile_path}`}
-              alt={`${data.name} poster`}
-              width={2000}
-              height={3000}
-            />
-            <div className={styles.rest_card}>
-              <Image
-                src={`https://image.tmdb.org/t/p/original/${data.profile_path}`}
-                alt={`${data.name} cover`}
-                width={5000}
-                height={3000}
-              />
-              <div className={styles.cont}>
-                <h4>{data.name}</h4>
-                <div className={styles.sub}>
-                  <p>{data.character}</p>
-                </div>
-              </div>
-            </div>
-            </Link>
-        ))}
+    <Link
+      className={styles.card}
+      href={`/actor/${data.id}-${toURL(data.name)}`}
+    > 
+      <Image
+      className={styles.poster}
+      src={`https://image.tmdb.org/t/p/original/${data.profile_path}`}
+      alt={`${data.name} poster`}
+      width={2000}
+      height={3000}
+    />
+    <div className={styles.rest_card}>
+      {/* <Image
+        src={`https://image.tmdb.org/t/p/original/${data.profile_path}`}
+        alt={`${data.name} cover`}
+        width={5000}
+        height={3000}
+      /> */}
+      <div className={styles.cont}>
+        <h4>{data.name}</h4>
+        <div className={styles.sub}>
+          <p>{data.character}</p>
+        </div>
       </div>
-    </section>
+    </div>
+    </Link>
   )
 }
